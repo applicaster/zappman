4142881c5920b356cae0f221f34f8d9dbc1301ce
gavriguy <gavri.guy@gmail.com>
2022-11-19 15:51:09 +0200

    Update time-ago.tsx
    clearInterval

--- src/components/time-ago.tsx
+++ src/components/time-ago.tsx
@@ -7,9 +7,10 @@ dayjs.extend(relativeTime);
 export default function TimeAgo({ timestamp }: { timestamp: number }) {
   const [now, setNow] = useState(Date.now());
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setNow(Date.now());
     }, 60_000);
+    return clearInterval(interval);
   }, [timestamp]);
   return (
     <React.Fragment key={now}>{dayjs(timestamp).fromNow()}</React.Fragment>
